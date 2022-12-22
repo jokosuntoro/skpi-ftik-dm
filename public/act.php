0a48c16b0c41757d30b525fdbf75d02e8e13929d
Joko Suntoro <jokosunt@gmail.com>
2022-12-23 03:38:40 +0700

    php dataming naive bayes

---
+++ public/act.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    <title>PHP Data Mining</title>
+</head>
+
+<body>
+    <?php
+    error_reporting(0);
+    require '../vendor/autoload.php';
+
+    use jokodm\Datamining\Dataset\Demo\PembelianKomputer;
+    use jokodm\Datamining\Klasifikasi\NaiveBayes;
+
+    $usia = $_POST['usia'];
+    $pendapatan = $_POST['pendapatan'];
+    $pelajar = $_POST['pelajar'];
+    $kredit = $_POST['kredit'];
+
+    $dataset = new PembelianKomputer();
+
+    $klasifikasi = new NaiveBayes();
+    $klasifikasi->train($dataset->getSamples(), $dataset->getTargets());
+
+    $prediksi = $klasifikasi->predict([$usia, $pendapatan, $pelajar, $kredit]);
+
+    if ($prediksi == 'beli') {
+        $hasil = "<font color=blue>Beli Komputer</font>";
+    } else {
+        $hasil = "<font color=red>Tidak Beli Komputer</font>";
+    }
+
+    if ($usia == "" or $pendapatan == "" or $pelajar == "" or $kredit == "") {
+        echo "<div class='container mt-3'>
+        <p mb-2>Harap isi value pada atribut dengan lengkap</p>
+        <a href='index.php' class='btn btn-primary'>Kembali ke Index</a>
+        </div>";
+    } else {
+        echo "<div class='container mt-3'>
+        <table class=table table-striped table-bordered>
+            <thead>
+                <tr>
+                    <th scope=col>Atribut</th>
+                    <th scope=col>Value</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Usia</td>
+                    <td>$usia</td>
+                </tr>
+                <tr>
+                    <td>Pendapatan</td>
+                    <td>$pendapatan</td>
+                </tr>
+                <tr>
+                    <td>Pelajar</td>
+                    <td>$pelajar</td>
+                </tr>
+                <tr>
+                    <td>Kredit</td>
+                    <td>$kredit</td>
+                </tr>
+                <tr class=font-weight-bold>
+                    <td>Hasil Prediksi</td>
+                    <td>$hasil</td>
+                </tr>
+            </tbody>
+        </table>
+        <a href='index.php' class='btn btn-primary'>Kembali ke Index</a>
+    </div>";
+    }
+    ?>
+
+</body>
+
+</html>
---
+++ public/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <title>:: Pembelian Komputer ::</title>
+</head>
+
+<body>
+    <div class="container">
+        <h4 class="text-primary text-center mt-5">
+            Sistem Cerdas Prediksi Pembelian Komputer
+        </h4>
+        <form action="act.php" method="POST" class="form-horizontal">
+            <fieldset>
+                <div class="form-row mt-3">
+                    <div class="form-group col-md-12">
+                        <label class="col-form-label">Usia</label>
+                        <div>
+                            <select name="usia" class="form-control">
+                                <option value="++" selected>--Pilih Usia--</option>
+                                <option value="muda">Muda</option>
+                                <option value="tengah_baya">Tengah Baya</option>
+                                <option value="tua">Tua</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label class="col-form-label">Pendapatan</label>
+                        <div>
+                            <select name="pendapatan" class="form-control">
+                                <option value="++">--Pilih Pendapatan--</option>
+                                <option value="muda">Rendah</option>
+                                <option value="tengah_baya">Sedang</option>
+                                <option value="tua">Tinggi</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label class="col-form-label">Pelajar</label>
+                        <div>
+                            <select name="pelajar" class="form-control">
+                                <option value="++" selected>--Apakah Pelajar?--</option>
+                                <option value="ya">Ya</option>
+                                <option value="tidak">Tidak</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label class="col-form-label">Kredit</label>
+                        <div>
+                            <select name="kredit" class="form-control">
+                                <option value="++">--Pilih -Status Kredit-</option>
+                                <option value="lancar">Lancar</option>
+                                <option value="macet">Macet</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group mt-5">
+                    <button type="submit" class="btn btn-primary btn-md mr-2">Proses</button>
+                    <button type="reset" class="btn btn-danger btn-md">Cancel</button>
+                </div>
+            </fieldset>
+        </form>
+    </div>
+</body>
+
+</html>
